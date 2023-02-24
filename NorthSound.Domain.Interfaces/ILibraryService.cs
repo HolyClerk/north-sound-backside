04ed44c75edbe614151acca58d1ef6bfd21c08a9
Dmitry Kovalev <phppriest@gmail.com>
2023-02-24 18:41:18 +0500

    Перенос интерфейса библиотеки в кор-слой

---
+++ NorthSound.Domain.Interfaces/ILibraryService.cs
@@ -0,0 +1,16 @@
+﻿using NorthSound.Domain.Entities;
+
+namespace NorthSound.Domain.Interfaces;
+
+public interface ILibraryService
+{
+    public const int MaxSizeOfFile = 20971520; // 20 MB
+    public const int MinSizeOfFile = 20971;
+    public const string AudioContentType = "audio/mpeg";
+
+    IEnumerable<Song> GetSongs();
+    Task<Song?> GetSongAsync(int id);
+    FileStream GetFileStream(Song entity);
+    Task<Song> CreateSongAsync(Song entity, Stream stream);
+    Task DeleteAsync(int id);
+}
