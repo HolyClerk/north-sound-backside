c8a467d410f5db6b0c99e10b5dd00de429f46360
Dmitry Kovalev <phppriest@gmail.com>
2023-02-25 18:38:41 +0500

    POCO для получения файлов из бизнес-логики

---
+++ NorthSound.Backend.Domain/Entities/SongFile.cs
@@ -0,0 +1,17 @@
+﻿namespace NorthSound.Backend.Domain.Entities;
+
+public class SongFile
+{
+    public SongFile() { }
+
+    public SongFile(string name, FileStream fileStream, string contentType)
+    {
+        Name = name;
+        FileStream = fileStream;
+        ContentType = contentType;
+    }
+
+    public string Name { get; set; }
+    public FileStream FileStream { get; set; }
+    public string ContentType { get; set; }
+}
