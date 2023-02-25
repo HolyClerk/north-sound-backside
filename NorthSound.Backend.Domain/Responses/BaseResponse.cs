337054bb9df2d02e5b89e0f86ffe397b7db71c95
Dmitry Kovalev <phppriest@gmail.com>
2023-02-25 17:30:45 +0500

    Создание Base Response для бизнеса

---
+++ NorthSound.Backend.Domain/Responses/BaseResponse.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+
+namespace NorthSound.Backend.Domain.Responses;
+
+public class BaseResponse <T>
+{
+    public ResponseStatus Status { get; set; } 
+    public T ResponseData { get; set; } = default!;
+}
+
+public enum ResponseStatus
+{
+    Success,
+    NotFound,
+    BadRequest
+}
