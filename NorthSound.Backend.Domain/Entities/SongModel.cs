9d40678a05e56a4fa3d072ddc242b36241ed9833
Dmitry Kovalev <phppriest@gmail.com>
2023-02-25 19:24:49 +0500

    Добавление POCO для отправки клиенту

---
+++ NorthSound.Backend.Domain/Entities/SongModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NorthSound.Backend.Domain.Entities;
+
+public class SongModel
+{
+    public SongModel(Song song)
+    {
+        Id = song.Id;
+        Name = song.Name;
+        Author = song.Author;
+    }
+
+    public int Id { get; set; } = default;
+    public string Name { get; set; } = default!;
+    public string Author { get; set; } = default!;
+}
--- NorthSound.Backend.Domain/Responses/BaseResponse.cs
+++ NorthSound.Backend.Domain/Responses/BaseResponse.cs
@@ -12,5 +12,6 @@ public enum ResponseStatus
 {
     Success,
     NotFound,
-    BadRequest
+    BadRequest,
+    Failed
 }
