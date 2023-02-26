e5df56723af467906b5542bf7b964e86629f732c
Dmitry Kovalev <phppriest@gmail.com>
2023-02-26 21:21:17 +0500

    init репозиторий для таблицы с пользователям

---
+++ NorthSound.Backend.DAL/Abstractions/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using NorthSound.Backend.Domain.Entities;
+
+namespace NorthSound.Backend.DAL.Abstractions;
+
+public interface IUserRepository
+{
+    IEnumerable<User> GetUsers();
+    Task<User?> GetUserAsync(string username, string password);
+    User CreateAsync(User entity);
+    User UpdateAsync(User entity);
+    User DeleteAsync(int id);
+    User SaveAsync();
+}
---
+++ NorthSound.Backend.DAL/UserRepository.cs
@@ -0,0 +1,37 @@
+﻿using NorthSound.Backend.DAL.Abstractions;
+using NorthSound.Backend.Domain.Entities;
+
+namespace NorthSound.Backend.DAL;
+
+public class UserRepository : IUserRepository
+{
+    public User CreateAsync(User entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public User DeleteAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<User?> GetUserAsync(string username, string password)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<User> GetUsers()
+    {
+        throw new NotImplementedException();
+    }
+
+    public User SaveAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public User UpdateAsync(User entity)
+    {
+        throw new NotImplementedException();
+    }
+}
