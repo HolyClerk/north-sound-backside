a3da081eeb460b5fdc84eb4fe3be0b63fecda4bb
Dmitry Kovalev <phppriest@gmail.com>
2023-02-26 21:24:51 +0500

    Добавление нового слоя для сервисов зависящих от библиотек

---
+++ NorthSound.Backend.Infrastructure/StorageGenerator.cs
@@ -0,0 +1,16 @@
+﻿using NorthSound.Backend.Services.Abstractions;
+using System.Security.Cryptography;
+
+namespace NorthSound.Backend.Infrastructure;
+
+public class StorageGenerator : IStorageGenerator
+{
+    public string GenerateStoragePath()
+    {
+        string generatedName = RandomNumberGenerator
+            .GetInt32(-2_147_483_640, 2_147_483_640)
+            .ToString();
+
+        return @$"Z:\Storage\{generatedName}";
+    }
+}
---
+++ NorthSound.Backend.Infrastructure/TokenHandler.cs
@@ -0,0 +1,12 @@
+﻿using NorthSound.Backend.Domain.Entities;
+using NorthSound.Backend.Services.Abstractions;
+
+namespace NorthSound.Backend.Infrastructure;
+
+public class TokenHandler : ITokenHandler
+{
+    public string GenerateToken(User user)
+    {
+        throw new NotImplementedException();
+    }
+}
