3d4a38e37a22301b6f82c1b23548856feb82be3a
Dmitry Kovalev <phppriest@gmail.com>
2023-02-27 19:53:21 +0500

    Новый контроллер для получения токенов

---
+++ NorthSound.Backend.LibraryApplication/Controllers/AuthorizationController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NorthSound.Backend.Domain.Responses;
+using NorthSound.Backend.Services;
+using NorthSound.Backend.Services.Abstractions;
+
+namespace NorthSound.Backend.LibraryApplication.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthorizationController : ControllerBase
+{
+    private readonly IUserService _userService;
+
+    public AuthorizationController(
+        IUserService userService)
+    {
+        _userService = userService;
+    }
+
+    // GET: api/authorization/
+    [HttpGet]
+    public async Task<ActionResult> Get(string username, string password)
+    {
+        var response = await _userService.AuthenticateAsync(username, password);
+
+        if (response.Status is not ResponseStatus.Success)
+            return ValidationProblem();
+
+        return Ok(response.ResponseData);
+    }
+}
