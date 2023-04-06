e5ba5afb3a989e61edf7c05e3279dd958447819d
Dmitry Kovalev <phppriest@gmail.com>
2023-04-06 13:31:17 +0500

    Перенос потокового воспроизведения в новый контроллер

---
+++ NorthSound.Backend.LibraryApplication/Controllers/StreamController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NorthSound.Backend.Domain.Responses;
+using NorthSound.Backend.Services.Abstractions;
+
+namespace NorthSound.Backend.LibraryApplication.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class StreamController : ControllerBase
+{
+    private readonly ILibraryService _library;
+
+    public StreamController(ILibraryService library)
+    {
+        _library = library;
+    }
+
+    // GET: api/library/5
+    [HttpGet("{id}")]
+    public async Task<ActionResult<FileStream>> Get(int id)
+    {
+        var response = await _library.GetSongFileAsync(id);
+
+        if (response.Status is not ResponseStatus.Success)
+            return BadRequest();
+
+        return File(
+            response.ResponseData.FileStream,
+            "audio/mpeg");
+    }
+}
