db231edb8509a36356abfe1a9da7bce7b5faecc1
Dmitry Kovalev <phppriest@gmail.com>
2023-04-13 16:02:13 +0500

    Размещение кода в Program.cs на верхний уровень

--- NorthSound.Backend.LibraryApplication/Program.cs
+++ NorthSound.Backend.LibraryApplication/Program.cs
@@ -6,67 +6,60 @@ using NorthSound.Backend.Services;
 using NorthSound.Backend.Services.Abstractions;
 using NorthSound.Backend.Infrastructure;
 
-namespace NorthSound.Backend.LibraryApplication;
-
-public class Program
+void ConnectDatabase(WebApplicationBuilder builder)
 {
-    public static void Main(string[] args)
-    {
-        var builder = WebApplication.CreateBuilder(args);
+    string? connection = builder.Configuration.GetConnectionString("PgConnection");
 
-        builder.Services.AddAuthentication("OAuth")
-            .AddJwtBearer("OAuth", config =>
-            {
-                var tokenGen = new JwtTokenGenerator();
+    if (connection is null)
+        throw new Exception("Wrong connection");
 
-                config.TokenValidationParameters = new TokenValidationParameters()
-                {
-                    ValidIssuer = JwtTokenGenerator.Issuer,
-                    ValidAudience = JwtTokenGenerator.Audience,
-                    IssuerSigningKey = tokenGen.SecurityKey
-                };
-            });
-
-        builder.Services.AddControllers();
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
-
-        ConnectDatabase(builder);
-
-        builder.Services
-            .AddTransient<IStorageGenerator, StorageGenerator>()        // Класс, необходимый для создания путей
-            .AddTransient<ITokenHandler, JwtTokenGenerator>()           // Сервис работы с токенами
-            .AddScoped<IAsyncSongRepository, AsyncSongRepository>()     // Репозиторий музыки
-            .AddScoped<ILibraryService, LibraryService>()               // Сервис, работающий с репо музыки
-            .AddScoped<IUserRepository, UserRepository>()               // Репо пользователей
-            .AddScoped<IAuthenticateService, AuthenticateService>();    // Сервис, работающий с авторизацией пользователей
-
-        var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
-
-        app.UseHttpsRedirection();
-
-        app.UseAuthorization();
-        app.UseAuthentication();
-
-        app.MapControllers();
-
-        app.Run();
-    }
-
-    public static void ConnectDatabase(WebApplicationBuilder builder)
-    {
-        string? connection = builder.Configuration.GetConnectionString("PgConnection");
-
-        if (connection is null)
-            throw new Exception("Wrong connection");
-
-        builder.Services.AddDbContext<ApplicationContext>(options => options.UseNpgsql(connection));
-    }
+    builder.Services.AddDbContext<ApplicationContext>(options => options.UseNpgsql(connection));
 }
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddAuthentication("OAuth")
+    .AddJwtBearer("OAuth", config =>
+    {
+        var tokenGen = new JwtTokenGenerator();
+
+        config.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidIssuer = JwtTokenGenerator.Issuer,
+            ValidAudience = JwtTokenGenerator.Audience,
+            IssuerSigningKey = tokenGen.SecurityKey
+        };
+    });
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+ConnectDatabase(builder);
+
+builder.Services
+    .AddTransient<IStorageGenerator, StorageGenerator>()        // Класс, необходимый для создания путей
+    .AddTransient<ITokenHandler, JwtTokenGenerator>()           // Сервис работы с токенами
+    .AddScoped<IAsyncSongRepository, AsyncSongRepository>()     // Репозиторий музыки
+    .AddScoped<ILibraryService, LibraryService>()               // Сервис, работающий с репо музыки
+    .AddScoped<IUserRepository, UserRepository>()               // Репо пользователей
+    .AddScoped<IAuthenticateService, AuthenticateService>();    // Сервис, работающий с авторизацией пользователей
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+app.UseAuthentication();
+
+app.MapControllers();
+
+app.Run();
