9bf620142b502e110165a56a8ad9493423b1a642
Dmitry Kovalev <phppriest@gmail.com>
2023-05-11 01:52:45 +0500

    Новые DTO

---
+++ NorthSound.Backend.Domain/Entities/DialogueDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NorthSound.Backend.Domain.Entities;
+
+public class DialogueDTO
+{
+    [Key] public int Id { get; set; } = default!;
+
+    [Required] public UserDTO FirstUser { get; set; } = default!;
+
+    [Required] public UserDTO SecondUser { get; set; } = default!;
+
+    [Required] public DateTime CreatedAt { get; set; } = default!;
+}
---
+++ NorthSound.Backend.Domain/Entities/MessageDTO.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NorthSound.Backend.Domain.Entities;
+
+public class MessageDTO
+{
+    [Key] public int Id { get; set; } = default!;
+
+    [ForeignKey(nameof(Dialogue))] public DialogueDTO Dialogue { get; set; } = default!;
+
+    [ForeignKey(nameof(Sender))] public UserDTO Sender { get; set; } = default!;
+
+    [ForeignKey(nameof(Receiver))] public UserDTO Receiver { get; set; } = default!;
+
+    [Required] public string Message { get; set; } = default!;
+
+    [Required] public DateTime CreatedAt { get; set; } = default!;
+}
