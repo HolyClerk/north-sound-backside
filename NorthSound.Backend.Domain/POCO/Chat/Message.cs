8153dfd618300fff975b3b9a41ac76a7241e5e96
Holy Clerk <phppriest@gmail.com>
2023-05-12 01:16:08 +0500

    Новый POCO

---
+++ NorthSound.Backend.Domain/POCO/Chat/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthSound.Backend.Domain.POCO.Chat;
+
+public class Message
+{
+    public ChatUser Sender { get; set; }
+    public ChatUser Receiver { get; set; }
+
+    public string MessageData { get; set; }
+}
