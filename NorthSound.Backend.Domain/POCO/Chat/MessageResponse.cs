e621937af3e97d96cf533d05c9433e48f7d09976
Holy Clerk <phppriest@gmail.com>
2023-05-12 22:30:15 +0500

    Изменение моделей запросов и ответов для чата

---
+++ NorthSound.Backend.Domain/POCO/Chat/MessageResponse.cs
@@ -0,0 +1,37 @@
+﻿using NorthSound.Backend.Domain.Entities;
+using NorthSound.Backend.Domain.Responses;
+
+namespace NorthSound.Backend.Domain.POCO.Chat;
+
+public class MessageResponse
+{
+    public ChatUser Sender { get; set; }
+
+    public ChatUser Receiver { get; set; }
+
+    public Message Message { get; set; }
+
+    public string Details { get; set; }
+
+    public ResponseStatus Status { get; set; }
+
+    public static MessageResponse Success(ChatUser sender, ChatUser receiver, Message message)
+    {
+        return new MessageResponse
+        {
+            Sender = sender,
+            Receiver = receiver,
+            Message = message,
+            Status = ResponseStatus.Success
+        };
+    }
+
+    public static MessageResponse Failed(string details)
+    {
+        return new MessageResponse
+        {
+            Details = details,
+            Status = ResponseStatus.Failed
+        };
+    }
+}
