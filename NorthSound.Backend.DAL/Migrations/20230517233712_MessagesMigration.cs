dad6be20fde273351a95f85853d2a8547237a892
Holy Clerk <phppriest@gmail.com>
2023-05-18 04:42:58 +0500

    миграции

---
+++ NorthSound.Backend.DAL/Migrations/20230517233712_MessagesMigration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NorthSound.Backend.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class MessagesMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Message",
+                table: "Messages",
+                newName: "Text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Text",
+                table: "Messages",
+                newName: "Message");
+        }
+    }
+}
