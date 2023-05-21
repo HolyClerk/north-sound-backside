474a3aaba176e85eb70c46a29b88da5afa661214
Dmitry Kovalev <phppriest@gmail.com>
2023-05-21 05:52:41 +0500

    Добавление кэширования в приложение

--- NorthSound.Backend.LibraryApplication/Program.cs
+++ NorthSound.Backend.LibraryApplication/Program.cs
@@ -42,6 +42,7 @@ builder.Services.AddSignalR();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddMemoryCache();
 
 ConnectDatabase(builder);
 
