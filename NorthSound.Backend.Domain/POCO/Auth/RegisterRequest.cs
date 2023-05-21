456ca242326910ba2d363ae6d0cc2f22c680d1e3
Dmitry Kovalev <phppriest@gmail.com>
2023-05-21 06:53:46 +0500

    Добавление вьюмоделей

--- NorthSound.Backend.Domain/POCO/Auth/RegisterRequest.cs
+++ NorthSound.Backend.Domain/POCO/Auth/RegisterRequest.cs
@@ -5,14 +5,9 @@ namespace NorthSound.Backend.Domain.POCO.Auth;
 
 public class RegisterRequest
 {
-    [Required] public string Username { get; set; }
-
-    [Required]
-    [DataType(DataType.EmailAddress)]
-    [EmailAddress]
+    public string Username { get; set; }
     public string Email { get; set; }
-
-    [Required] public string Password { get; set; }
+    public string Password { get; set; }
 
     public User MapToUser()
     {
---
+++ NorthSound.Backend.Domain/ViewModels/AuthenticateViewModel.cs
@@ -0,0 +1,19 @@
+﻿using NorthSound.Backend.Domain.POCO.Auth;
+using System.ComponentModel.DataAnnotations;
+
+namespace NorthSound.Backend.Domain.ViewModels;
+
+public class AuthenticateViewModel
+{
+    [Required] public string Username { get; set; }
+    [Required] public string Password { get; set; }
+
+    public AuthenticateRequest MapToRequest()
+    {
+        return new AuthenticateRequest
+        {
+            Username = Username,
+            Password = Password,
+        };
+    }
+}
