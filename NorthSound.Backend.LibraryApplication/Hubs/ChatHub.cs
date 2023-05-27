2357c0a9ba08ea823a41e0e22ebf21d3706dd29b
Dmitry Kovalev <phppriest@gmail.com>
2023-05-27 22:21:02 +0500

    Добавление нового функционала хабу

--- NorthSound.Backend.LibraryApplication/Hubs/ChatHub.cs
+++ NorthSound.Backend.LibraryApplication/Hubs/ChatHub.cs
@@ -40,15 +40,42 @@ public class ChatHub : Hub<IChatHub>
             return;
         }
 
-        _logger.LogInformation("Пользователь добавлен в чат: {}", response.Data!.CurrentUser.Id);
+        var username = response.Data!.CurrentUser.Name;
+
+        _logger.LogInformation("Пользователь добавлен в чат: {}", username);
+
+        await Clients.All.ReceiveConnectectionNotification(new ChatUserDTO(username));
         await base.OnConnectedAsync();
     }
 
     public override async Task OnDisconnectedAsync(Exception? exception)
     {
+        var response = _chatService.GetChatUser(Context.ConnectionId);
+
+        if (response.Status is not ResponseStatus.Success)
+            return;
+
+        var username = response.Data!.CurrentUser.Name;
+
         _chatService.RemoveChatUser(Context.ConnectionId);
-        _logger.LogInformation("Пользователь отключен {}", Context.ConnectionId);
+        _logger.LogInformation("Пользователь отключен {}", username);
+
+        await Clients.All.ReceiveDisconnectedNotification(new ChatUserDTO(username));
         await base.OnDisconnectedAsync(exception);
+    }
+
+    public async Task GetAllClients()
+    {
+        var response = _chatService.GetChatUsers();
+        var dtoList = new List<ChatUserDTO>();
+
+        foreach (var chatUser in response)
+        {
+            dtoList.Add(new ChatUserDTO(chatUser.CurrentUser.Name));
+        }
+
+        await Clients.Client(Context.ConnectionId)
+            .ReceiveClients(dtoList);
     }
 
     public async Task SendMessage(MessageViewModel viewModel)
@@ -66,8 +93,7 @@ public class ChatHub : Hub<IChatHub>
 
         _logger.LogInformation("Сообщение сформировано удачно {}", response.Message.Value);
         
-        await Clients
-            .Client(response.Receiver.Connection.Id)
+        await Clients.Client(response.Receiver.Connection.Id)
             .ReceiveMessage(response.Sender.CurrentUser.Name, response.Message.Value);
     }
 }
--- NorthSound.Backend.LibraryApplication/Hubs/IChatHub.cs
+++ NorthSound.Backend.LibraryApplication/Hubs/IChatHub.cs
@@ -1,6 +1,11 @@
-﻿namespace NorthSound.Backend.LibraryApplication.Hubs;
+﻿using NorthSound.Backend.Domain.POCO.Chat;
+
+namespace NorthSound.Backend.LibraryApplication.Hubs;
 
 public interface IChatHub
 {
     Task ReceiveMessage(string username, string message);
+    Task ReceiveClients(IEnumerable<ChatUserDTO> chatUsers);
+    Task ReceiveDisconnectedNotification(ChatUserDTO disconnectedUser);
+    Task ReceiveConnectectionNotification(ChatUserDTO connectedUser);
 }
