b7680093641b8cc88664e43e6138e83fa4f78821
Holy Clerk <phppriest@gmail.com>
2023-06-01 16:26:53 +0500

    Session Builder

---
+++ NorthSound.Backend.Services/Builders/SessionBuilder.cs
@@ -0,0 +1,36 @@
+﻿using NorthSound.Backend.Domain.Entities;
+using NorthSound.Backend.Domain.POCO.Chat;
+
+namespace NorthSound.Backend.Services.Builders;
+
+public class SessionBuilder
+{
+    private readonly Session _session;
+
+    public SessionBuilder()
+    {
+        _session = new Session();
+    }
+
+    public SessionBuilder SetUser(User user)
+    {
+        _session.CurrentUser = user;
+        return this;
+    }
+
+    public SessionBuilder CreateConnection(string connectionId)
+    {
+        _session.Connection = new ChatConnection
+        {
+            CreatedAt = DateTime.Now,
+            Id = connectionId,
+        };
+
+        return this;
+    }
+
+    public Session Build()
+    {
+        return _session;
+    }
+}
